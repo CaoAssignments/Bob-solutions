d4962b9749dc0c318a89a4b63548b8bfa9b5b181
Bob Zhang <chz026@ucsd.edu>
2021-01-19 21:49:40 -0800

    shell game reading lines

--- shell_bronze_jan19/Solution.java
+++ shell_bronze_jan19/Solution.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Solution {
     public static void main (String[] args) {
-        System.out.println("Hello world!");
+        int fileIdx = Integer.parseInt(args[0]);
+        try {
+            Scanner s = new Scanner(new File("./" + fileIdx + ".in"));
+            String cur = s.nextLine();
+            String[] swaps = new String[Integer.parseInt(cur)];
+            for (int i = 0; i < swaps.length; i++) {
+                swaps[i] = s.nextLine();
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
