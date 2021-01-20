3ca5bb0ab2ee8f083cf4aedfd3c58aab8036d24f
Bob Zhang <chz026@ucsd.edu>
2021-01-19 23:15:21 -0800

    done with shell swap. mostly correct with guess animal

--- guess_bronze_jan19/Solution.java
+++ guess_bronze_jan19/Solution.java
@@ -1,5 +1,51 @@
+import java.util.Set;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+
 public class Solution {
+    private static final int SECOND_IN_ARRAY = 2;
+
+    private static void mapify (int fileIdx, HashMap<String, HashSet<String>> characs) {
+        try {
+            Scanner s = new Scanner(new File("./" + fileIdx + ".in"));
+            String cur = s.nextLine();
+            String[] splitted = null;
+            String currName = null;
+            int iters = Integer.parseInt(cur);
+            for (int i = 0; i < iters; i++) {
+                splitted = s.nextLine().split(" ");
+                currName = splitted[0];
+                characs.put(currName, new HashSet<String>());
+                for (int j = SECOND_IN_ARRAY; j < splitted.length; j++) {
+                    characs.get(currName).add(splitted[j]);
+                }
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
     public static void main (String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String, HashSet<String>> characs = new HashMap<String, HashSet<String>>();
+        mapify(Integer.parseInt(args[0]), characs);
+        Object[] keys = characs.keySet().toArray();
+        int result = 0;
+        int curr = 0;
+        for (int i = 0; i < keys.length; i++) {
+            for (int j = i + 1; j < keys.length; j++) {
+                HashSet<String> seti = characs.get(keys[i]);
+                HashSet<String> setj = characs.get(keys[j]);
+                for (String s : seti) {
+                    if (setj.contains(s)) {
+                        curr += 1;
+                    }
+                }
+            }
+            if (curr > result) result = curr;
+        }
+        System.out.println("Answer: " + (result + 1));
     }
 }
--- shell_bronze_jan19/Solution.java
+++ shell_bronze_jan19/Solution.java
@@ -1,20 +1,68 @@
+/**
+ * Author: Bob Zhang
+ * CSE 199 Winter 2021 - Shell Game Solution
+ */
+
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Solution {
-    public static void main (String[] args) {
-        int fileIdx = Integer.parseInt(args[0]);
+    private static final int DIGITS_PER_LINE = 3;
+    private static final int LAST_DIGIT_IDX = 2;
+
+    // store data for processing in check()
+    private static int[] swap1, swap2, guess;
+
+    /**
+     * parse data into int[] for further processing
+     * @param fileIdx: range from 1 to 10, corresponding to 10 test cases
+     */
+    private static void arraylize (int fileIdx) {
         try {
             Scanner s = new Scanner(new File("./" + fileIdx + ".in"));
             String cur = s.nextLine();
-            String[] swaps = new String[Integer.parseInt(cur)];
-            for (int i = 0; i < swaps.length; i++) {
-                swaps[i] = s.nextLine();
+            String[] nums = new String[DIGITS_PER_LINE];
+            swap1 = new int[Integer.parseInt(cur)];
+            swap2 = new int[Integer.parseInt(cur)];
+            guess = new int[Integer.parseInt(cur)];
+            for (int i = 0; i < swap1.length; i++) {
+                cur = s.nextLine();
+                nums = cur.split(" ");
+
+                // store 3 numbers in respective arrays
+                swap1[i] = Integer.parseInt(nums[0]);
+                swap2[i] = Integer.parseInt(nums[1]);
+                guess[i] = Integer.parseInt(nums[LAST_DIGIT_IDX]);
             }
             s.close();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    /**
+     * assume target starts at 'starting' index, check all the way through
+     * @param starting: assume where the target starts at
+     * @return points when target was initially at this index
+     */
+    private static int check (int starting) {
+        int result = 0, curr = starting;
+        for (int i = 0; i < swap1.length; i++) {
+            if (swap1[i] == curr) curr = swap2[i];
+            else if (swap2[i] == curr) curr = swap1[i];
+            if (curr == guess[i]) result++;
+        }
+        return result;
+    }
+
+    public static void main (String[] args) {
+        int fileIdx = Integer.parseInt(args[0]);
+        arraylize(fileIdx);
+        int maxPossible = 0;
+        for (int i = 1; i <= DIGITS_PER_LINE; i++) {
+            maxPossible = Integer.max(maxPossible, check(i));
+        }
+        System.out.println("Maximum possible points is: " + maxPossible);
     }
 }
