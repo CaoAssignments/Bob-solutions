ee12b9222739fd8383be36c979c31c7f3e5987f0
Bob <chz026@ucsd.edu>
2021-05-07 17:41:03 -0700

    updated cowjog solution to test on USACO. passed

--- cowjog_bronze_dec14/Solution.java
+++ cowjog_bronze_dec14/Solution.java
@@ -1,19 +1,23 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Solution {
     public static void main (String[] args) {
-        int fileIdx = Integer.parseInt(args[0]);
+        // int fileIdx = Integer.parseInt(args[0]);
+        int result = 0;
         try {
-            Scanner s = new Scanner(new File("./" + fileIdx + ".in"));
+            // Scanner s = new Scanner(new File("./" + fileIdx + ".in"));
+            Scanner s = new Scanner(new File("cowjog.in"));
             int lines = Integer.parseInt(s.nextLine());
             int[] speeds = new int[lines];
             for (int i = 0; i < speeds.length; i++) {
                 speeds[i] = Integer.parseInt(s.nextLine().split(" ")[1]);
             }
             int end = speeds[speeds.length - 1];
-            int result = 1;
+            result = 1;
             for (int i = speeds.length - 2; i >= 0; i--) {
                 if (speeds[i] <= end) {
                     result++;
@@ -21,8 +25,15 @@ public class Solution {
                 end = Math.min(end, speeds[i]);
             }
             s.close();
-            System.out.println(result + " groups remain");
+            // System.out.println(result);
         } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileWriter out = new FileWriter("cowjog.out");
+            out.write(result + "");
+            out.close();
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
