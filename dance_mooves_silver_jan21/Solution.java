0379edcb8777293879a85b69f50d499fb629441e
Bob Zhang <chz026@ucsd.edu>
2021-05-28 09:28:14 -0700

    slight optimization but doesn't help

--- dance_mooves_silver_jan21/Solution.java
+++ dance_mooves_silver_jan21/Solution.java
@@ -5,10 +5,14 @@ public class Solution {
         return Integer.parseInt(s);
     }
 
-    private static final boolean ok (int[] arr) {
+    private static final boolean ok (int swaps, int k, int[] arr) {
+        if (swaps % k != 0)
+            return false;
+
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != i) return false;
         }
+
         return true;
     }
 
@@ -16,8 +20,11 @@ public class Solution {
         // read input
         Scanner s = new Scanner(System.in);
         String line = s.nextLine();
-        int n = toInt(line.split(" ")[0]);
-        int k = toInt(line.split(" ")[1]);
+        final int n = toInt(line.split(" ")[0]);
+        final int k = toInt(line.split(" ")[1]);
+
+        // track how many swaps have happened
+        int swaps = 0;
 
         // make a set for each cow to track locs it's been to
         // their indices start at 1, so +1 to avoid off by one errors
@@ -58,12 +65,14 @@ public class Solution {
             // add each other to their set of "been to" sets (no duplicate)
             cows[fst].add(Integer.valueOf(snd));
             cows[snd].add(Integer.valueOf(fst));
+
+            swaps++;
         }
         s.close();
 
         Iterator<String> itr = lines.iterator();
         // simulate until it's back in original state
-        while (!ok(cowLocs)) {
+        while (!ok(swaps, k, cowLocs)) {
             if (!itr.hasNext()) {
                 itr = lines.iterator(); // go back to start
             }
@@ -84,6 +93,8 @@ public class Solution {
             // add each other to their set of "been to" sets (no duplicate)
             cows[fst].add(Integer.valueOf(snd));
             cows[snd].add(Integer.valueOf(fst));
+
+            swaps++;
         }
 
         // print results
