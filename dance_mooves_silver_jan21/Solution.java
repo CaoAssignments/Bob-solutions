749a3308d491bcec9627b5913b15dc6fdc84fc71
Bob Zhang <chz026@ucsd.edu>
2021-05-28 10:04:41 -0700

    solution

--- dance_mooves_silver_jan21/Solution.java
+++ dance_mooves_silver_jan21/Solution.java
@@ -1,10 +1,61 @@
 import java.util.*;
+import java.io.*;
 
 public class Solution {
     private static final int toInt (String s) {
         return Integer.parseInt(s);
     }
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer = new StringTokenizer(in.readLine());
+        int n = toInt(tokenizer.nextToken());
+        int k = toInt(tokenizer.nextToken());
+        int[] cows = new int[n + 1];
+        List<Integer>[] viewed = new List[n + 1];
+        for (int j = 1; j <= n; j++) {
+            cows[j] = j;
+            viewed[j] = new ArrayList<>();
+            viewed[j].add(j);
+        }
+        for (int t = 1; t <= k; t++) {
+            tokenizer = new StringTokenizer(in.readLine());
+            int a = toInt(tokenizer.nextToken());
+            int b = toInt(tokenizer.nextToken());
+            int c = cows[a];
+            int d = cows[b];
+            cows[a] = d;
+            cows[b] = c;
+            viewed[cows[a]].add(a);
+            viewed[cows[b]].add(b);
+        }
+        int[] answer = new int[n + 1];
+        for (int r = 1; r <= n; r++) {
+            if (cows[r] != 0) {
+                List<Integer> cycle = new ArrayList<>();
+                int j = r;
+                while (cows[j] != 0) {
+                    cycle.add(j);
+                    j = cows[j];
+                    cows[cycle.get(cycle.size() - 1)] = 0;
+                }
+                Set<Integer> viewedHere = new HashSet<>();
+                for (int cow : cycle) {
+                    viewedHere.addAll(viewed[cow]);
+                }
+                for (int cow : cycle) {
+                    answer[cow] = viewedHere.size();
+                }
+            }
+        }
+        StringBuilder out = new StringBuilder();
+        for (int j = 1; j <= n; j++) {
+            out.append(answer[j]).append('\n');
+        }
+        System.out.print(out);
+    }
+
+    /*
     private static final boolean ok (int swaps, int k, int[] arr) {
         if (swaps % k != 0)
             return false;
@@ -102,4 +153,5 @@ public class Solution {
             System.out.println(cows[i].size());
         }
     }
+    */
 }
